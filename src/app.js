53aa6bb9eda817a1efdb6fbdb29d11b3b0e59008
Prince Verma <16836599+vprince001@users.noreply.github.com>
2019-01-16 13:08:56 +0530

    Introduce readfile to server for homepage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
+const fs = require("fs");
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+  try {
+    fs.readFile("." + req.url, function(err, contents) {
+      res.statusCode = 200;
+      res.write(contents);
+      res.end();
+      if (err) {
+        res.end();
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = app;
