8aa3277b18788ba128b4a7f0292f3d2e831f88c5
Prince Verma <16836599+vprince001@users.noreply.github.com>
2019-01-17 11:44:06 +0530

    Watering cane will hide on mouse click
    Watering cane will hide for 1 second.

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const fs = require("fs");
 const app = (req, res) => {
   try {
     fs.readFile("." + req.url, function(err, contents) {
+      if (err) {
+        res.end();
+        return;
+      }
       res.statusCode = 200;
       res.write(contents);
       res.end();
-      if (err) {
-        res.end();
-      }
     });
   } catch (err) {
     console.log(err);
